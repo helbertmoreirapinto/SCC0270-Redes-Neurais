c6c9dff48c6cbaf8bd63429fb3133006deb543f1
Helbert M Pinto <helbert.tec.info@gmail.com>
2022-03-31 00:18:44 -0300

    finalizado

---
+++ exercicio1/readme.txt
@@ -0,0 +1,15 @@
+SCC0270 - Redes Neurais e Aprendizado Profundo  
+
+Alunos:  
+- 10716504 - Helbert Moreira Pinto
+- 10377708 - João Marcos Della Torre Divino
+
+Exercicio 1 - Implementar e treinar o modelo Adaline para reconhecer os símbolos Y e Y invertido (letra “Y” e letra “Y” invertida)
+
+
+Para a realização deste exercicio, desenvolvemos os seguintes códigos, que encontram-se na pasta "src": 
+- perceptron.py -> implementação do neuronio  
+- modelos.py -> código auxiliar para tratar da geração/tratamento/visualização dos dados
+
+
+Criamos um jupyter notebook contendo um breve resumo da implementação e execução do exercicio.
--- exercicio1/src/modelos.py
+++ exercicio1/src/modelos.py
@@ -10,115 +10,118 @@ def gerar_dados(n=50, num_ruido=3):
     if num_ruido <= 0:
         num_ruido = 1
     
-
     # modelos padrão
-    x = []
-    x.append(np.array([
+    modelos_padrao = []
+    modelos_padrao.append(np.array([
         [+1, -1, -1, -1, +1],
         [+1, -1, -1, -1, +1],
         [-1, +1, +1, +1, -1],
         [-1, -1, +1, -1, -1],
         [-1, +1, +1, +1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [+1, -1, -1, -1, +1],
         [-1, +1, -1, +1, -1],
         [-1, -1, +1, -1, -1],
         [-1, -1, +1, -1, -1],
         [-1, +1, +1, +1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [+1, -1, -1, -1, +1],
         [-1, +1, -1, +1, -1],
         [-1, -1, +1, -1, -1],
         [-1, -1, +1, -1, -1],
         [-1, -1, +1, -1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [+1, -1, -1, -1, +1],
         [+1, +1, -1, +1, +1],
         [-1, +1, +1, +1, -1],
         [-1, -1, +1, -1, -1],
         [-1, -1, +1, -1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [-1, +1, -1, +1, -1],
         [-1, +1, -1, +1, -1],
         [-1, +1, +1, +1, -1],
         [-1, -1, +1, -1, -1],
         [-1, -1, +1, -1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [+1, +1, -1, +1, +1],
         [-1, +1, +1, +1, -1],
         [-1, -1, +1, -1, -1],
         [-1, -1, +1, -1, -1],
         [-1, +1, +1, +1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [+1, -1, -1, -1, +1],
         [+1, -1, -1, -1, +1],
         [+1, +1, +1, +1, +1],
         [-1, -1, +1, -1, -1],
         [-1, -1, +1, -1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [+1, -1, -1, -1, +1],
         [+1, -1, -1, -1, +1],
         [-1, +1, -1, +1, -1],
         [-1, -1, +1, -1, -1],
         [-1, -1, +1, -1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [-1, +1, -1, +1, -1],
         [-1, +1, -1, +1, -1],
         [-1, +1, -1, +1, -1],
         [-1, +1, +1, +1, -1],
         [-1, -1, +1, -1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [-1, -1, -1, -1, -1],
         [+1, +1, -1, +1, +1],
         [-1, +1, +1, +1, -1],
         [-1, -1, +1, -1, -1],
         [-1, +1, +1, +1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [+1, +1, -1, +1, +1],
         [-1, +1, -1, +1, -1],
         [-1, +1, +1, +1, -1],
         [-1, -1, +1, -1, -1],
         [-1, -1, +1, -1, -1]
     ]))
-    x.append(np.array([
+    modelos_padrao.append(np.array([
         [-1, +1, -1, +1, -1],
         [-1, +1, -1, +1, -1],
         [-1, -1, +1, -1, -1],
         [-1, -1, +1, -1, -1],
         [-1, -1, -1, -1, -1]
     ]))
-
+    
     # adiciona os modelos de Y invertido utilizando o comando flip
-    for i in range(len(x)):
-        x.append(np.flip(x[i]))
+    for m in range(len(modelos_padrao)):
+        modelos_padrao.append(np.flip(modelos_padrao[m]))
     
     # transforma as matrizes dos modelos em listas utilizando o comando flatten
     # cria uma lista com os valores esperados (variavel resposta)
-    x = [xi.flatten() for xi in x]
-    y = [1 if i < len(x)/2 else -1 for i in range(len(x))]
+    modelos_padrao = [modelo_padrao.flatten() for modelo_padrao in modelos_padrao]
+    y = [1 if i < len(modelos_padrao)/2 else -1 for i in range(len(modelos_padrao))]
 
+    # salvando imagens dos modelos padrão
+    for modelo_padrao_i, modelo_padrao in enumerate(modelos_padrao):
+        gerar_grafico('./modelos', modelo_padrao_i, modelo_padrao)
+    
     # nomeando colunas do dataframe de retorno
     col = ['bias']              # bias
-    for i in range(1, len(x[0])+1):
+    for i in range(1, len(modelos_padrao[0])+1):
         col.append(f'x{i}')     # xn
     col.append('y')             # y
 
     # cria o dataframe de retorno
     dataframe = pd.DataFrame([],columns=col)
 
-    # criando novo diretorio para armazenamento dos modelos com ruido
-    dir = './modelos'
-    subdir = [x[0] for x in os.walk(dir)]
+    # criando novo diretorio para armazenamento dos modelos gerados com ruido
+    dir = './gerados'
+    subdir = [p[0] for p in os.walk(dir)]
     exec = 0 if len(subdir) == 0 else len(subdir) -1
     diretorio = f'{dir}/exec_{exec}'
     os.makedirs(diretorio)
@@ -128,24 +131,26 @@ def gerar_dados(n=50, num_ruido=3):
         ni = len(dataframe)
 
         # seleciona um modelo randomicamente
-        idx_sel = np.random.randint(len(x))
-        sel_x = x[idx_sel]
+        indice_modelo = np.random.randint(len(modelos_padrao))
+        modelo_escolhido = modelos_padrao[indice_modelo].copy()
 
         # seleciona e aplica ruído ao modelo
-        idx_ruido = np.random.choice(len(sel_x), size=np.random.randint(num_ruido+1), replace=False)
-        for idx in idx_ruido:
-            sel_x[idx] *= -1
+        array_indices_ruido = np.random.choice(len(modelo_escolhido), size=1+np.random.randint(num_ruido), replace=False)
+        for indice in array_indices_ruido:
+            modelo_escolhido[indice] *= -1
         
         # gerando e salvando imagem do modelo com ruido
-        gerar_grafico(diretorio, ni, sel_x)
+        str_arr_ruido = '_'.join([str(a) for a in array_indices_ruido])
+        nome_img = f'img_{ni}_modelo_{indice_modelo}_ruido_{str_arr_ruido}'
+        gerar_grafico(diretorio, nome_img, modelo_escolhido)
         
         # insere o valor 1 ao inicio (bias)
         # insere o label resposta no fim da lista
-        values = np.insert(sel_x, 0, 1)
-        values = np.append(values, y[idx_sel])
+        modelo_ruido = np.insert(modelo_escolhido, 0, 1)
+        modelo_ruido = np.append(modelo_ruido, y[indice_modelo])
 
         # adiciona o modelo com ruído aplicado ao dataframe de retorno
-        dataframe.loc[-1] = values
+        dataframe.loc[-1] = modelo_ruido
         dataframe.index = dataframe.index + 1
         dataframe = dataframe.sort_index()
         dataframe = dataframe.drop_duplicates(ignore_index=True)
@@ -154,29 +159,30 @@ def gerar_dados(n=50, num_ruido=3):
 
 
 
-def gerar_grafico(dir, idx_img, valores):
+def gerar_grafico(dir, nome_img, valores):
     plt.ioff()
-    fig = plt.figure(figsize=(2,2))
+    fig = plt.figure(figsize=(5,5))
 
     # adiciona as bolinhas pretas (valores -1 do modelo - representação do vazio)
     base_x = [[x for x in range(5)] for y in range(5)]
     base_y = [[y for x in range(5)] for y in range(5)]
-    plt.scatter(base_x, base_y, color='#000000', marker='o', s=300)
+    plt.scatter(base_y, base_x, color='#FFFFFF', marker='s', s=2250)
     
     # adiciona as bolinhas laranja (valores 1 do modelo - representação de conteudo)
     pos_x = []
     pos_y = []
     for i, v in enumerate(valores):
         if v == 1:
-            pos_x.append(int(i%5))
-            pos_y.append(int(i/5))
-    plt.scatter(pos_x, pos_y, color='#F4B084', marker='o', s=300)
+            pos_x.append(4 - int(i/5))
+            pos_y.append(4 - int(i%5))
+    plt.scatter(pos_y, pos_x, color='#000000', marker='s', s=2250)
     
     # removendo eixos
     ax=plt.gca()
+    plt.axis([-1, 5, -1, 5])
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
     
     # salvando imagem
-    plt.savefig(f'{dir}/modelo_{idx_img}.png')
+    plt.savefig(f'{dir}/{nome_img}.png')
     plt.close(fig)
